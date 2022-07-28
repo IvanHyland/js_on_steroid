a55f3baf725f546ac3e51c1d1702ab9f6b30f83e
Ivan Rodriguezcalvo <Ivan.Rodriguezcalvo@hyland.com>
2022-07-28 11:14:07 -0500

    spread and map practice

--- app.ts
+++ app.ts
@@ -1,2 +1,28 @@
-let hello: string = "hey from Typescript world"
-let myName:string = "Ivan R."
+console.clear()
+
+let hello: string = 'hey from Typescript world'
+let myName:string = 'Ivan R.'
+
+let greeting = (user:string) =>{
+  console.log(`${hello} ${user}`)
+}
+
+let student1 = ['John', '29', 'programmer', 'Playing games']
+let student2 = ['Lewis', '32', 'engineer', 'riding horses']
+let student3 = ['Meagan', '27', 'hair stylist', 'dancing']
+
+//spread operator
+let person = [...student1, 'Juan', '30', "manager", "computing", ...student2]
+
+//the Map object
+let employee = new Map();
+
+employee.set('manager', 'Lauren Mckinney')
+employee.set('supervisor', 'Paul Don')
+employee.set('employee1', 'Ivan Shepard')
+employee.set('developer', 'Eric Pit')
+employee.set('product owner', 'Maria DoSantos')
+
+
+
+console.log(employee.get('supervisor'))
